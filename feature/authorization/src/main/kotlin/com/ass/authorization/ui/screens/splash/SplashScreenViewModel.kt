f7e43e56c60d24f97d4e54de34382b8daa225235
Dennis <kotlinkod@gmail.com>
2024-12-14 14:26:18 +0400

    made splashScreen

--- feature/authorization/src/main/kotlin/com/ass/authorization/ui/screens/splash/SplashScreenViewModel.kt
+++ feature/authorization/src/main/kotlin/com/ass/authorization/ui/screens/splash/SplashScreenViewModel.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-private const val SPLASH_SCREEN_DELAY = 10000L
+private const val SPLASH_SCREEN_DELAY = 100L
 
 class SplashScreenViewModel : BaseViewModel<SplashScreenUiState>() {
     init {
